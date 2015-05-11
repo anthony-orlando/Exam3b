1c9e6215a9a9ce7d2d96d362df4a39ef910fd54c
Anthony Orlando <anthony-orlando@uiowa.edu>
2015-05-10 19:27:32 -0600

    checked for user errors

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -19,15 +19,22 @@ Partial Class _Default
         btn_submit.Visible = False
         rb_gender.Visible = False
 
-        Dim dateGiven As DateTime = Calendar1.SelectedDate
-        Dim name As String = tb_name.Text
-        Dim earn As Decimal = tb_earn.Text
-        Dim gender As String = rb_gender.SelectedValue
-
-        lbl_nameR.Text = name
-        lbl_date.Text = dateGiven
-        lbl_earnR.Text = String.Format("{0:c}", earn)
-        lbl_genderr.Text = gender
+        If tb_name.Text <> "" Then
+            Dim name As String = tb_name.Text
+            lbl_nameR.Text = name
+        End If
+        If tb_earn.Text <> "" Then
+            Dim earn As Decimal = tb_earn.Text
+            lbl_earnR.Text = String.Format("{0:c}", earn)
+        End If
+        If Calendar1.SelectedDate <> Nothing Then
+            Dim dateGiven As DateTime = Calendar1.SelectedDate
+            lbl_date.Text = dateGiven
+        End If
+        If rb_gender.SelectedValue <> Nothing Then
+            Dim gender As String = rb_gender.SelectedValue
+            lbl_genderr.Text = gender
+        End If
 
     End Sub
 End Class
